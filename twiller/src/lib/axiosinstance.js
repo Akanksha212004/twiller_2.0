2cd84de59bcdccc622bcdf249147ed9c43202121
Akanksha Yadav <akankshayadav212004@gmail.com>
2026-03-02 22:50:03 +0530

    Fixed axios issue

--- twiller/src/lib/axiosinstance.js
+++ twiller/src/lib/axiosinstance.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: process.env.BACKEND_URL,
-    // headers: {
-        //     "Content-Type":"application/json"
-        // }
-    // baseURL: "http://localhost:5000",
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    withCredentials: true,
+  baseURL: "https://twiller-2-0-t0ez.onrender.com",
+  withCredentials: true,
 });
 
 export default axiosInstance;
